99d368cb6a66c8dc0fd9ef04d44bc746acb8ce73
JackHamby <jackhamby@JackHambys-MacBook-Pro.local>
2022-02-19 17:37:28 -0600

    hopefully his doenst mess stuff up

--- backend/dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorCode = void 0;
-var ErrorCode;
-(function (ErrorCode) {
-    ErrorCode[ErrorCode["unknown"] = 0] = "unknown";
-    ErrorCode[ErrorCode["loginFailed"] = 1] = "loginFailed";
-})(ErrorCode = exports.ErrorCode || (exports.ErrorCode = {}));
-//# sourceMappingURL=types.js.map
